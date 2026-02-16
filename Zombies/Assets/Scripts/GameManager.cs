f6fdf7ae8a3c55d270bfc6ab828136e69a3d419d
BeateBerkmane <bberkmane@gmail.com>
2026-02-16 10:35:10 +0200

    added izmera maiņu

--- Zombies/Assets/Scripts/GameManager.cs
+++ Zombies/Assets/Scripts/GameManager.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class GameManager : MonoBehaviour
 {
     public GameObject selectedZombie;
     public GameObject [] zombies;
     public Vector3 selectedSize;
+    private InputAction left, right;
+    private int selectedIndex;
+    
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        selectZombie(2);
+        selectZombie(0);
+        left = InputSystem.actions.FindAction("PrewZombie");
+        right = InputSystem.actions.FindAction("NextZombie");
         //Debug.Log("selected" + selectedZombie.name);
     }
 
     void selectZombie(int index)
     {
+        if (selectedZombie != null)
+            selectedZombie.transform.localScale = Vector3.one;
         selectedZombie = zombies[index];
         selectedZombie.transform.localScale = selectedSize;
         Debug.Log("selected" + selectedZombie.name);
@@ -22,6 +30,19 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (left.WasReleasedThisFrame())
+        {
+            selectedIndex--;
+            selectZombie(selectedIndex);
+            Debug.Log("Left is pressed");
+            
+        }
+        if (right.WasPressedThisFrame())
+        {
+            selectedIndex++;
+            selectZombie(selectedIndex);
+            Debug.Log("Right is pressed");
+            
+        }
     }
 }
